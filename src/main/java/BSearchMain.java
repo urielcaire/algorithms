37da78cd7029dd4ed03d8c365ced5ad35a283737
urielcaire <uriel.caire@gmail.com>
2020-02-04 17:13:50 -0300

    add BSearchMain main class

--- src/main/java/BSearchMain.java
+++ src/main/java/BSearchMain.java
@@ -2,7 +2,7 @@ package main.java;
 
 import main.java.model.BinarySearch;
 
-public class Main {
+public class BSearchMain {
     public static void main(String[] args) {
         int v[] = {32,67,89};
         int target = 67;
