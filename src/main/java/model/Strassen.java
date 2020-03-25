9a11bb19a0a5d608ccd51f6314a94ad6fd6f8574
urielcaire <uriel.caire@gmail.com>
2020-03-25 02:56:50 -0300

    Matrix multiplication

--- src/main/java/model/Strassen.java
+++ src/main/java/model/Strassen.java
@@ -1,7 +1,13 @@
 package main.java.model;
 
 public class Strassen {
-    public Strassen(){}
+    public Strassen(){
+        // TO DO
+    }
+
+    private void divAndConquerMM(int[][] m1, int[][] m2, int n){
+        // TO DO
+    }
 
     public int[][] simpleMatrixMultiplication(int[][] m1, int[][] m2){
         int m3[][] = new int[m1.length][m2[0].length];
